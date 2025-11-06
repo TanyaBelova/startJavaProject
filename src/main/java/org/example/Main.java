a89c90c51ef7f708963fb3fee68bef4f52dba232
=Tatyana Gavrilova <=>
2025-11-06 23:50:05 +0300

    Вынесение логики фильтрации в отдельный метод. Удаление комментариев. Реформатирование

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import books.Book;
 import books.Writer;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -16,7 +17,7 @@ public class Main {
         Writer writer1 = new Writer("Миллер", "Генри", null, "26.12.1891", "США", "Английский");
         Writer writer2 = new Writer("Достоевский", "Фёдор", "Михайлович", "30.10.1821", "Россия", "Русский");
         Writer writer3 = new Writer("Айтматов", "Чингиз", "Торекулович", "12.12.1928", "Кыргызстан", "Русский");
-        Writer writer4 = new Writer("Бакман", "Фредерик", null , "02.06.1981", "Швеция", "Шведский");
+        Writer writer4 = new Writer("Бакман", "Фредерик", null, "02.06.1981", "Швеция", "Шведский");
 
         Book book1 = new Book("Тропик любви", "Роман, драма", "Английский", "123-456-789", "1936", "Апрель", "Россия, Москва", "2011", 960, writer1);
         Book book2 = new Book("Братья Карамазовы", "Роман", "Русский", "125-478-5456", "1878", "МосКнига", "Россия, Москва", "1880", 500, writer2);
@@ -28,12 +29,6 @@ public class Main {
         Book book8 = new Book("Вторая жизнь Уве", "Роман", "Английский", "124-589-5454", "2012", "Simon&Schuster", "Нью-йорк", "2012", 555, writer4);
         Book book9 = new Book("Бабушка велела кланяться и просить прощения", "Роман", "Русский", "455-589-5454", "2013", "Синдбад", "Россия, Москва", "2018", 400, writer4);
         Book book10 = new Book("Медвежий угол", "Роман", "Шведский", "485-189-5454", "2016", "Piratförlaget ", "Швеция", "2017", 658, writer4);
-
-
-//        System.out.println(book1 + "\n");
-//        System.out.println(book2 + "\n");
-//        System.out.println(book3 + "\n");
-//        System.out.println(book4 + "\n");
 
         ArrayList<Book> bookList = new ArrayList<>();
         bookList.add(book1);
@@ -47,56 +42,30 @@ public class Main {
         bookList.add(book9);
         bookList.add(book10);
 
-//        jsonSerialize(bookList);
+        jsonSerialize(bookList);
 
         jsonDeserialize();
-
-
-//        for (int i = 0; i<bookList.size(); i++){
-//            if(!bookList.get(i).getLanguage().equals("Русский")){
-//               bookList.remove(i);
-//            }
-//        }
-
-//        for (Book element: bookList){
-//            if(!element.getLanguage().equals("Русский")){
-//                bookList.remove(element);
-//            }
-//        }
-//        System.out.println(bookList);
     }
-    public static void jsonDeserialize() throws IOException {
 
-//        StringBuilder result = new StringBuilder();
-//
-//        try (BufferedReader br = new BufferedReader(new FileReader("D:/Backend/startJavaProject/src/main/resources/jsonFile.txt"))) {
-//            String line;
-//
-//            while((line = br.readLine()) != null) {
-//                result.append(line);
-//                result.append('\n');
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        String jsonString = result.toString();
-       // System.out.println(jsonString);
+    public static void jsonDeserialize() throws IOException {
 
         File file = new File("D:/Backend/startJavaProject/src/main/resources/jsonFile.txt");
 
-        List<Book> booksFromJsonList = new ObjectMapper().readValue(file, new TypeReference<>(){});
+        List<Book> booksFromJsonList = new ObjectMapper().readValue(
+                file, new TypeReference<>() {});
         System.out.println(booksFromJsonList);
     }
 
-        public static void jsonSerialize(ArrayList<Book> bookList) throws JsonProcessingException {
-            List rusultBookList = bookList.stream()
-                    .filter(book -> book.getLanguage().equals("Русский"))
-                    .collect(Collectors.toList());
+    public static void jsonSerialize(ArrayList<Book> bookList) throws JsonProcessingException {
 
-            ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-            String jsonBookList = mapper.writeValueAsString(rusultBookList);
-            System.out.println(jsonBookList);
-       }
+        ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+        String jsonBookList = mapper.writeValueAsString(bookFilter(bookList, "Английский"));
+        System.out.println(jsonBookList);
+    }
+
+    public static List<Book> bookFilter(ArrayList<Book> bookList, String language) {
+        return bookList.stream().filter(book -> book.getLanguage().equals(language)).collect(Collectors.toList());
+    }
 
 }
 
